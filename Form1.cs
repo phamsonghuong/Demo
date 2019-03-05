c53abef976f00f9bc63b8976ea1ef8eaab1df7ca
phamsonghuong <1554050046huong@ou.edu.vn>
2019-03-05 11:03:26 +0700

    XU LY TINH TOAN

--- Form1.cs
+++ Form1.cs
@@ -15,5 +15,27 @@ namespace Test1
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void text1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(text1.Text);
+            int b = int.Parse(text2.Text);
+            int c = a + b;
+        }
     }
 }
