c2ddfb3628dc54648a846e5aa234a0cf537a9aa4
phamsonghuong <1554050046huong@ou.edu.vn>
2019-03-05 10:23:02 +0700

    THIET KE GIAO DIEN

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,36 +29,45 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.text1 = new System.Windows.Forms.TextBox();
+            this.text2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(167, 42);
+            this.button1.Location = new System.Drawing.Point(154, 137);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // text1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(60, 45);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 17);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "text1";
+            this.text1.Location = new System.Drawing.Point(40, 33);
+            this.text1.Name = "text1";
+            this.text1.Size = new System.Drawing.Size(141, 22);
+            this.text1.TabIndex = 1;
+            this.text1.TextChanged += new System.EventHandler(this.text1_TextChanged);
+            // 
+            // text2
+            // 
+            this.text2.Location = new System.Drawing.Point(40, 80);
+            this.text2.Name = "text2";
+            this.text2.Size = new System.Drawing.Size(141, 22);
+            this.text2.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(282, 255);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.text2);
+            this.Controls.Add(this.text1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -67,7 +76,8 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox text1;
+        private System.Windows.Forms.TextBox text2;
     }
 }
 
